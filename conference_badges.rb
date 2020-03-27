03cd161aab2892552ae5e31d7d55fe1b8c7ea0d0
Mat Cabral <mathewcabral@gmail.com>
2020-03-27 20:33:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,29 +1,29 @@
+# Write your code here.
+
 def badge_maker(name)
-  return "Hello, my name is #{name}."
-end# Write your code here.
-
-
-def batch_badge_creator(array)
-  badge_messages = []
-  array.each do |name|
-    badge_messages << "Hello, my name is #{name}."
-  end
-  badge_messages
+  
+  "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(array)
+  badge_messeges = []
+  array.each do |name|
+    badge_messeges << "Hello, my name is #{name}."
+  end
+  badge_messeges
+end
 def assign_rooms(speakers)
   rooms_array = []
+  
   speakers.each do |name|
-    rooms_array << "Hello, #{name}! You'll be assigned to room #{speakers.index(name) + 1}"
+    rooms_array << "Hello, #{name}! You'll be assigned to room #{speakers.index(name) + 1}!"
   end
   rooms_array
 end
+
 def printer(speakers)
   batch_badge_creator(speakers).each do |greeting|
-  puts greeting
-end
-assign_rooms(speakers).each do |greeting|
-  puts greeting
-end
-
+    puts greeting
+  end
+  
 end
